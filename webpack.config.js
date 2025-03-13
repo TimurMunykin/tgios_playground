58e9beee19e32b6ed5f845d9f09f5de5e56c7395
TimurPersonalMac <timur.munykin@gmail.com>
2025-03-13 18:04:08 +0100

    tmp changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -26,8 +28,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/styles.css', to: 'styles.css' },
+      ],
+    }),
   ],
   devServer: {
+    host: '192.168.31.228', // Listen on all interfaces
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
